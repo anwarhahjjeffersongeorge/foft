a75d78366d536d2ac961c0727e75e3d328fcc1be
Anwar Hahj Jefferson-George <anwarhahjjeffersongeorge@gmail.com>
2018-09-29 15:16:28 -0700

    src/mocha/mathoft.js Add dummy test

---
+++ src/mocha/mathoft.mocha.js
@@ -0,0 +1,27 @@
+/**
+* Test suite for ../mathoft.js
+*/
+
+import chai from 'chai';
+import chai_arrays from 'chai-arrays';
+import chai_as_promised from 'chai-as-promised';
+chai.use(chai_as_promised);
+chai.use(chai_arrays);
+chai.config.includeStack = true; // turn on stack trace
+var should = chai.should();
+var expect = chai.expect;
+var assert = chai.assert;
+//
+import * as MathOfT from '../mathoft.js';
+// var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+  });
+});
+
+
+//TODO browser tests
+//TODO nodejs tests
