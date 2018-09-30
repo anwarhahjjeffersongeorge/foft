ec60f100ee8cb09b6089ad1008cd996a4d2d9a5b
Anwar Hahj Jefferson-George <anwarhahjjeffersongeorge@gmail.com>
2018-09-30 00:56:02 -0700

    delete test

--- test/main.js
+++
@@ -1,9 +0,0 @@
-"use strict"; // ESM syntax is supported.
-
-var _mathoftjs = _interopRequireDefault(require("./mathoftjs"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-module.exports = {
-  MathOfT: _mathoftjs.default
-};
