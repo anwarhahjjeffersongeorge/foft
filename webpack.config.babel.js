8a9f5d73898c52877203c0279572869df561000e
Anwar Hahj Jefferson-George <anwarhahjjeffersongeorge@gmail.com>
2018-12-02 21:59:15 -0800

    build process

---
+++ webpack.config.babel.js
@@ -0,0 +1,20 @@
+import '@babel/polyfill'
+const path = require('path');
+
+module.exports = {
+  entry: './src/mathoft.js',
+  mode:'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module:{
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    }]
+  },
+};
