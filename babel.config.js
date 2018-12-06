d7ba4ac0811cc18b2aa2da855550c03a1804be0e
Anwar Hahj Jefferson-George <anwarhahjjeffersongeorge@gmail.com>
2018-12-05 22:53:20 -0800

    build process change naming: devtest -> disttest
    build process change babel:
    polyfill -> plugin-transform-runtime

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,22 @@
 module.exports = function (api){
   let presets = [
-    "@babel/preset-env"
+    [
+      "@babel/preset-env", {
+        "useBuiltIns": "entry"
+      }
+    ]
   ];
   let plugins = [
-    "@babel/plugin-proposal-class-properties"
+    "@babel/plugin-proposal-class-properties",
+    [
+      "@babel/plugin-transform-runtime",
+      {
+        "corejs": false,
+        "helpers": true,
+        "regenerator": true,
+        "useESModules": false
+      }
+    ]
   ];
 
   if(api.env('test')){
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 "use strict";
 // ESM syntax is supported.
-import '@babel/polyfill'
 import * as M from './mathoft.js';
 module.exports = M;
